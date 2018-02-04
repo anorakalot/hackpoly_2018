25c5034ce478012076f9adf97bbe3be5a5984e33
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2018-02-04 10:31:25 -0800

    Add files via upload

--- 1_2_rough.py
+++ 1_2_rough.py
@@ -15,6 +15,10 @@ import operator
 import math
 import os
 from collections import defaultdict
+import pandas as pd
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 #keys and token for api acess
 consumer_key = 'UPI6kKkPaNIHoj8a2AafnhIEA'
@@ -88,6 +92,18 @@ def pre_process(s):
     tokens = tknzr.tokenize(tweet)
     return tokens
 
+def convert65536(s):
+    #Converts a string with out-of-range characters in it into a string with codes in it.
+    l=list(s);
+    i=0;
+    while i<len(l):
+        o=ord(l[i]);
+        if o>65535:
+            l[i]="{"+str(o)+"ū}";
+        i+=1;
+    return "".join(l);
+
+
 #makes a list of all string punctuations plus some other things that have no meaning in tweets
 
 def print_data(file_to_open):
@@ -95,7 +111,7 @@ def print_data(file_to_open):
 
 
     punctuation = list(string.punctuation)
-    other_unimportants = ['rt','via','Via','RT','、','、' , '…','」','...','’','「','️','。','“','”','🇷','🇺','__','『','ा','..']
+    other_unimportants = ['rt','via','Via','RT','、','、' , '…','」','...','’','「','️','。','“','”','🇷','🇺','__','『','ा','..','	🔥']
 
 
     title ="10 most common terms "
@@ -119,7 +135,7 @@ def print_data(file_to_open):
 
             #makes a list of all the tokens  - most stopwords and puncutation and other nonmeaning things
 
-            terms_all_minus_stop = [term for term in pre_process(tweet) if term not in stop and not term.startswith(('#','@','@','@')) and  term[0:5] != 'https'] #and 'https:' not in term)]
+            terms_all_minus_stop = [convert65536(term) for term in pre_process(tweet) if term not in stop and not term.startswith(('#','@','@','@')) and  term[0:5] != 'https' ] #and 'https:' not in term)]
 
             #bigram for two words at the opposite ends of things in the stop list
             #terms_bigram = bigrams(stop)
@@ -138,10 +154,22 @@ def print_data(file_to_open):
 
             #this line below takes tweet text and turns it into "tokens" of words
             #tokens = pre_process(tknzr.tokenize(tweet['text']))
-        str_1 = str(count_all_minus_stop.most_common(10))#.strip['[]']
+        str_1 = str(count_all_minus_stop.most_common(10))
         msgbox(str_1,title)
         #str_2 = str_2.strip['[]']
         f.close()
+
+        plt.bar(range(len(count_all_minus_stop.most_common(10))),list(dict(count_all_minus_stop.most_common(10)).values()),align = 'center')
+        plt.xticks(range(len(count_all_minus_stop.most_common(10))),list(dict(count_all_minus_stop.most_common(10)).keys()))
+        plt.ylabel("Occurences")
+        plt.show()
+        '''
+        df = pd.DataFrame.from_dict(count_all_minus_stop.most_common(10))
+        #print(df.head())
+        df[0].value_counts().plot(kind='hist')
+        plt.axis([40, 160, 0, 100])
+        plt.show()
+        '''
 
 
     title_2 = "10 most common hashtags "
@@ -160,7 +188,7 @@ def print_data(file_to_open):
             #list of things that should not count as important words telling us about public sentiment
             stop = list(stopwords.words('english')) + punctuation+  other_unimportants
             #makes a list of all the tokens  - most stopwords and puncutation and other nonmeaning things
-            terms_all_minus_stop = [term for term in pre_process(tweet) if term not in stop and term.startswith(('#'))]
+            terms_all_minus_stop = [convert65536(term) for term in pre_process(tweet) if term not in stop and term.startswith(('#')) ]
             #makes a dicionary with the token and the value being how many times its been said
             count_all_hashtags.update(terms_all_minus_stop)
             #this line below takes tweet text and turns it into "tokens" of words
@@ -168,6 +196,10 @@ def print_data(file_to_open):
         str_2 = str(count_all_hashtags.most_common(10))
         msgbox(str_2,title_2)
         f.close()
+        plt.bar(range(len(count_all_hashtags.most_common(10))),list(dict(count_all_hashtags.most_common(10)).values()),align = 'center')
+        plt.xticks(range(len(count_all_hashtags.most_common(10))),list(dict(count_all_hashtags.most_common(10)).keys()))
+        plt.ylabel("Occurences")
+        plt.show()
 
     #opens and gives 5 most common term co occurences
     title_3 = ("10 most common term co-occurences ")
@@ -186,6 +218,7 @@ def print_data(file_to_open):
             else:
                 tweet = ""
             terms_only = [term for term in pre_process(tweet) if term not in stop and not term.startswith(('#','&','@')) and  term[0:5] != 'https']
+            #terms_only = [term for term in pre_process(tweet) if term not in stop and not term.startswith(('&','@')) and  term[0:5] != 'https']
             #builds matrix if in terms_only w1 and w2 are not equal
 
             for i in range(len(terms_only)-1):
@@ -204,8 +237,28 @@ def print_data(file_to_open):
         term_max = sorted(com_max,key = operator.itemgetter(1),reverse=True)
 
         str_3 = str(term_max[:10])
+
+        str_3 = convert65536(str_3)
+        title_3 = convert65536(title_3)
         msgbox(str_3,title_3)
 
+
+        tuple_term = list(zip(*term_max[:10]))[0]
+
+        count_occurences = list(zip(*term_max[:10]))[1]
+
+        x_position = np.arange(len(list(zip(*term_max[:10]))[0]))
+        plt.bar(range(len(tuple_term)), count_occurences,align = 'center')
+        plt.xticks(x_position,tuple_term)
+        plt.ylabel("Occurences")
+        plt.show()
+
+        '''
+        plt.bar(range(len(term_max[:10])),list(term_max[:10].values()),align = 'center')
+        plt.xticks(range(len(term_max[:10])),list(dict(count_all_minus_stop.most_common(10)).keys()))
+
+        plt.show()
+        '''
     #Starting Semantic analysis
     positive_vocab = []
     negative_vocab = []
@@ -269,11 +322,48 @@ def print_data(file_to_open):
     title_4 = "Top Semantically Positive terms in tweets"
     str_4 = str(top_pos)
     msgbox(str_4,title_4)
-
+    pos_term = list(zip(*top_pos))[0]
+    pos_rating = list(zip(*top_pos))[1]
+    x_position = np.arange(len(pos_term))
+    plt.bar(x_position,pos_rating,align = 'center')
+    plt.xticks(x_position,pos_term)
+    plt.ylabel("Positive Semantic Orientation Score")
+    plt.show()
 
     title_5 = "Top Semantically Negative terms in tweets"
     str_5 = str(top_neg)
     msgbox(str_5,title_5)
+
+    neg_term = list(zip(*top_neg))[0]
+    neg_rating = list(zip(*top_neg))[1]
+    x_position = np.arange(len(neg_term))
+    plt.bar(x_position,neg_rating,align = 'center')
+    plt.xticks(x_position,neg_term)
+    plt.ylabel("Negative Semantic Orientation Score")
+    plt.show()
+
+
+    with open(file_to_open,'r') as f:
+        geo_data = {
+        "type": "FeatureCollection",
+        "features":[]
+        }
+        for line in f:
+            tweet = json.loads(line)
+            if tweet['coordinates']:
+                geo_json_feature = {
+                "type": "Feature",
+                "geometry":tweet['coordinates'],
+                "properties":{
+                "text":tweet['text'],
+                "created_at": tweet['created_at']
+                }
+                }
+                geo_data['features'].append(geo_json_feature)
+    with open('geo_data.json','w') as fout:
+        fout.write(json.dumps(geo_data, indent = 4))
+        print("geo data json being written")
+
 
 choice = "dummy_value"
 while(choice != "exit"):
@@ -287,7 +377,7 @@ while(choice != "exit"):
         filter_str = str(enterbox("Which filter for your new_stream "))
         file_to_make_stream = str(enterbox("name of new file? (Beware adding in the same string for a previously existing file will erase that file!)"))
         #makes a new stream object
-        time_limit = int(integerbox("Please enter seconds to make for your new tweet stream "))
+        time_limit = int(enterbox("Please enter seconds to make for your new tweet stream "))
         twitter_stream = Stream(auth,MyListener(file_to_make_stream,time_limit))
         #makes the .json file filled with twitter json data
         twitter_stream.filter(languages=["en"],track=[filter_str])
@@ -301,20 +391,23 @@ while(choice != "exit"):
             file_to_open = str(enterbox("Which file do you want to analyze "))
             if (os.path.isfile(file_to_open)!= True):
                 msgbox("not a valid file name")
-
-
             else:
                 print_data(file_to_open)
                 checker = False
+
     elif (choice == "look_at_json_files"):
         checker = True
         while checker:
             file_to_look_at = str(enterbox("Which json file do you want to look at "))
-            if(os.path.isfile(file_to_look_at) != True):
-                msgbox("not a valid json file name")
+            if enterbox():
+                if(os.path.isfile(file_to_look_at) != True):
+                    msgbox("not a valid json file name")
+                else:
+                    filename = os.path.normcase(file_to_look_at)
+                    f = open(filename, "r")
+                    text = f.readlines()
+                    f.close()
+                    codebox(text)
+                    checker = False
             else:
-                filename = os.path.normcase(file_to_look_at)
-                f = open(filename, "r")
-                text = f.readlines()
-                f.close()
-                codebox(text )
+                checker=False
